f8dad1a24dac44beb4369b46b71e11fb2bf066a8
Sam Nilsson <82225892+taccck@users.noreply.github.com>
2023-01-17 19:39:17 +0100

    respawn and checkpoint system

--- Assets/Developer/Sam/CheckpointBehavior.cs
+++ Assets/Developer/Sam/CheckpointBehavior.cs
@@ -1,13 +1,32 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class CheckpointBehavior : MonoBehaviour
 {
+    public static Dictionary<int, Vector3> checkpoints = new Dictionary<int, Vector3>();
+
+    [SerializeField] private int number = 1;
+
+    private void Start()
+    {
+        checkpoints.Add(number, transform.position);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         bool isPlayer = other.TryGetComponent(out Alteruna.Avatar avatar);
         if (!isPlayer || !avatar.IsMe) return;
 
         PlayerRespawn respawn = other.GetComponent<PlayerRespawn>();
-        respawn.checkpoint = transform;
+        if(respawn.checkpoint == number - 1) //can't go back or skip checkpoints
+        {
+            respawn.checkpoint++;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, 1f);
     }
 }
--- Assets/Developer/Sam/PlayerRespawn.cs
+++ Assets/Developer/Sam/PlayerRespawn.cs
@@ -1,41 +1,64 @@
 using UnityEngine;
 using Alteruna;
+using Alteruna.Trinity;
 
 public class PlayerRespawn : MonoBehaviour
 {
-    [HideInInspector] public Transform checkpoint;
+    [HideInInspector] public int checkpoint;
 
     [SerializeField] float respawnAt = -10f;
 
-    private Alteruna.Avatar avatar;
+    private bool isMe;
     private CharacterController cc;
+    private Multiplayer multiplayer;
 
     private void Start()
     {
-        avatar = GetComponent<Alteruna.Avatar>();
+        multiplayer = FindObjectOfType<Multiplayer>();
+        multiplayer.RegisterRemoteProcedure("RespawnRPC", RespawnRPC);
+        isMe = GetComponent<Alteruna.Avatar>().IsMe;
         cc = GetComponent<CharacterController>();
 
-        if (!avatar.IsMe) return;
-        Multiplayer multiplayer = FindObjectOfType<Multiplayer>();
-        checkpoint = multiplayer.AvatarSpawnLocations[multiplayer.Me.Index];
+        if (!isMe) return;
+        Vector3 spawn = multiplayer.AvatarSpawnLocations[multiplayer.Me.Index].position;
+        CheckpointBehavior.checkpoints.Add(0, spawn);
     }
 
     private void FixedUpdate()
     {
-        if (!avatar.IsMe) return;
+        if (!isMe) return;
 
         if (transform.position.y < respawnAt)
         {
-            Respawn(checkpoint.position);
+            CallRespawn();
         }
     }
 
-    private void Respawn(Vector3 position) //use this for rcp???
+    private void RespawnRPC(ushort fromUser, ProcedureParameters parameters, uint callId, ITransportStreamReader processor)
+    {
+        float x = parameters.Get("X", 0);
+        float y = parameters.Get("Y", 0);
+        float z = parameters.Get("Z", 0);
+
+        Respawn(new(x, y, z));
+    }
+
+    private void Respawn(Vector3 position)
     {
         cc.enabled = false;
         transform.position = position;
-        //transform.rotation = checkpoint.rotation;
         cc.enabled = true;
         cc.Move(Vector3.zero);
+    }
+
+    private void CallRespawn()
+    {
+        ProcedureParameters parameters = new ProcedureParameters();
+        Vector3 position = CheckpointBehavior.checkpoints[checkpoint];
+        parameters.Set("X", position.x);
+        parameters.Set("Y", position.y);
+        parameters.Set("Z", position.z);
+        multiplayer.InvokeRemoteProcedure("RespawnRPC", UserId.All, parameters);
+        Respawn(position);
     }
 }
